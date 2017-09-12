9d34e896ba5b74b67576e9f45ef28ed933826278
yfcloud <zhaokui@yfcloud.com>
2017-09-12 15:12:09 +0800

    更新为project

---
+++ app/src/main/java/com/yunfan/project/TestActivity.java
@@ -0,0 +1,19 @@
+package com.yunfan.project;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.yunfan.net.K2Pagent;
+
+/**
+ * Created by 37917 on 2017/9/12 0012.
+ */
+
+public class TestActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        K2Pagent k2Pagent;
+    }
+}
