4ed1767ee2eef7a852ae642ff73d047b3e2a0554
yfcloud <zhaokui@yfcloud.com>
2017-09-12 17:03:55 +0800

    添加测试代码

---
+++ YfK2Pagent/src/main/java/com/yunfan/pagent/test.java
@@ -0,0 +1,13 @@
+package com.yunfan.pagent;
+
+import com.yunfan.net.K2Pagent;
+
+/**
+ * Created by 37917 on 2017/9/12 0012.
+ */
+
+public class test {
+    public static void test(){
+        K2Pagent k2Pagent;
+    }
+}
---
+++ myapplication/src/main/java/com/yunfan/kit/myapplication/MainActivity.java
@@ -0,0 +1,16 @@
+package com.yunfan.kit.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.yunfan.net.K2Pagent;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        K2Pagent k2Pagent;
+    }
+}
